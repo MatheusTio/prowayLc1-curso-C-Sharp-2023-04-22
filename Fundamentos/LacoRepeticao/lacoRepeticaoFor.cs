cb921bfd387a5dd49f358510b41375e511975a83
Matheus Eduardo do Nascimento <71780@PROWAY.TREINA>
2023-04-22 18:28:05 -0300

    Laço de Repetição For

--- Fundamentos/LacoRepeticao/lacoRepeticaoFor.cs
+++ Fundamentos/LacoRepeticao/lacoRepeticaoFor.cs
@@ -23,8 +23,19 @@ namespace Fundamentos.LacoRepeticao
         // metodo privado sem retorno e sem parametro
         private void Exemplo01()
         {
+            //int indice = 0;
+            //while (indice < 5) 
+            //{
+            //    Console.WriteLine("Oie");
+            //    //incrementando a variavel indice
+            //    indice = indice + 1;
 
-
+            
+            //for (declaraçao da variavel; condição para execução do for:incremento)
+            for (int indice = 0; indice < 5; indice = indice + 1)
+                {
+                Console.WriteLine("Oie");
+                }
             
         }
         // metodo privado sem retorno e sem parametro
